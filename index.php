2616535cb3715d3edc516ab4caedd6c060bbf152
iwatatedaizou <iwatate1421@sacos.co.jp>
2024-11-07 16:54:08 +0900

    TOPページ作成　初版リリース

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>TailwindCSS + daisyUI Sample</title>
+        <link href="https://cdn.jsdelivr.net/npm/daisyui@4.6.1/dist/full.min.css" rel="stylesheet" type="text/css" />
+        <script src="https://cdn.tailwindcss.com/3.4.1"></script>
+    </head> 
+    <body class="m-4">
+        
+        <div id="wrapper">
+            <div class="text-center m-4">
+            <!-- 「戻る」ボタンを紫色にする設定を追加 -->
+            <a href="contact.php" class="btn normal-case bg-blue-600 hover:bg-blue-900 text-white">お問合せボタン</a>
+        </div>
+        </div>
+    </body>
+</html>
